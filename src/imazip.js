5fbc66512d4ba06528cf390920a324b8f18284ee
ParkYoungWoong <thesecon@gmail.com>
2019-02-03 13:19:38 +0900

    Applied the classes.

--- src/imazip.js
+++ src/imazip.js
@@ -1,93 +1,80 @@
 import utils from './utils';
-import es6Promise from 'es6-promise'
+import es6Promise from 'es6-promise';
 import {
   defaults,
   defaultsForThumb
-} from './defaults'
+} from './defaults';
 
-es6Promise.polyfill()
+es6Promise.polyfill();
 
-let _opts,
-  _optsForThumb,
-  _img;
+class Imazip {
+  constructor(options) {
+    this.opts = Object.assign({}, defaults, options);
+    this.optsForThumb = Object.assign({}, defaultsForThumb, options.thumbnail);
 
-function _imageListener(resolve, reject) {
-  _img.addEventListener('load', event => {
-    _compressImage(resolve, reject);
-  });
-  _img.addEventListener('error', event => {
-    _compressImage(resolve, reject, event, true);
-  });
-}
+    this.img = document.createElement('img');
+    this.img.src = options.src;
 
-function _checkMimeType() {
-  let mimeType;
+    this.canvas = document.createElement('canvas');
+    this.context = this.canvas.getContext('2d');
 
-  if (/image\//.test(_opts.format)) {
-    mimeType = _opts.format;
-  } else {
-    switch (_opts.format) {
-      case 'png':
-        mimeType = 'image/png';
-        break;
-      case 'webp':
-        mimeType = 'image/webp';
-        break;
-      default:
-        // jpg, jpeg, etc..
-        mimeType = 'image/jpeg';
+    this.resolve = null;
+    this.reject = null;
+  }
+
+  _imageListeners() {
+    this.img.addEventListener('load', event => {
+      this._operator(event);
+    });
+    this.img.addEventListener('error', event => {
+      this._operator(event, true);
+    });
+  }
+
+  _compressor(options) {
+    const {
+      width,
+      height,
+      quality,
+      maxSize
+    } = options;
+    const filteredSize = utils.sizeFilter(width, height, this.img.naturalWidth, this.img.naturalHeight);
+    const adjustedSize = utils.resizeImage(filteredSize.width, filteredSize.height, maxSize);
+
+    this.canvas.width = adjustedSize.width;
+    this.canvas.height = adjustedSize.height;
+
+    this.context
+      .drawImage(this.img, 0, 0, adjustedSize.width, adjustedSize.height);
+
+    return this.canvas.toDataURL(utils.checkMimeType(this.opts.format), quality);
+  }
+
+  _operator(event, isError) {
+    if (isError) {
+      this.reject(event);
     }
+
+    const origin = this._compressor(this.opts);
+    const thumbnail = this.opts.thumbnail
+      ? this._compressor(this.optsForThumb)
+      : null;
+
+    this.resolve({
+      origin,
+      thumbnail
+    });
   }
 
-  return mimeType;
-}
-
-function _makeCopyImage(args) {
-  const {
-    width,
-    height,
-    quality,
-    maxSize
-  } = args;
-  const canvas = document.createElement('canvas');
-  const filteredSize = utils.sizeFilter(width, height, _img.naturalWidth, _img.naturalHeight);
-  const adjustedSize = utils.resizeImage(filteredSize.width, filteredSize.height, maxSize);
-  canvas.width = adjustedSize.width;
-  canvas.height = adjustedSize.height;
-  canvas
-    .getContext("2d")
-    .drawImage(_img, 0, 0, adjustedSize.width, adjustedSize.height);
-
-  return canvas.toDataURL(_checkMimeType(), quality);
-}
-
-function _compressImage(resolve, reject, event, isError) {
-  if (isError) {
-    reject(event);
+  run() {
+    return new Promise((resolve, reject) => {
+      this.resolve = resolve;
+      this.reject = reject;
+      this._imageListeners();
+    });
   }
-
-  const origin = _makeCopyImage(_opts);
-  const thumbnail = _opts.thumbnail
-    ? _makeCopyImage(_optsForThumb)
-    : null;
-
-  resolve({
-    origin,
-    thumbnail
-  });
 }
 
-function imazip(options) {
-  _opts = Object.assign({}, defaults, options);
-  _optsForThumb = Object.assign({}, defaultsForThumb, options.thumbnail);
-  _img = document.createElement('img');
-  _img.src = options.src;
-
-  console.log(_img);
-
-  return new Promise((resolve, reject) => {
-    _imageListener(resolve, reject);
-  });
-}
-
-export { imazip, imazip as default };
+export default function (options) {
+  return new Imazip(options).run()
+};
